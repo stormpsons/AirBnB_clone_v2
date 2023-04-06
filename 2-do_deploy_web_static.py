e9ca0ef7b8b196fe3a22bf4a5879d0db58ca6e70
1923k <soolekefuoe2@gmail.com>
2023-04-06 02:26:03 -0700

    0x10. HTTPS SSL

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.160.94.102', '54.197.78.250']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
